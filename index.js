0379c131ee5d382fa1721aa85270acb7e3711f4c
Mona Lisa Octocat <110023253+Bernkra96@users.noreply.github.com>
2023-09-05 22:06:44 +0200

    Uptate for color

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+import chalk from 'chalk';
 let numOfLine = 9;
 
 for (let i = 0; i < numOfLine; i++) {
-  console.log(
-    '###############################',
-    'background: #222; color: #bada55',
-  );
+  console.log('###############################');
 }
+
+console.log(chalk.blue('Hello world!'));
